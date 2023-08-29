7a1c223536f29d35be44dd57b6301ecda72de319
TheFictionRed <143562833+TheFictionRed@users.noreply.github.com>
2023-08-29 17:47:30 -0600

    Add files via upload

---
+++ Eva1_1_hola_mundo/src/eva1_1_hola_mundo/Eva1_1_hola_mundo.java
@@ -0,0 +1,20 @@
+/*
+ Practica de hola mundo
+ */
+package eva1_1_hola_mundo;
+
+import javax.swing.JOptionPane;
+
+
+public class Eva1_1_hola_mundo {
+
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo!!");
+        
+        JOptionPane.showMessageDialog(null,"hola mundo!!");
+       
+    }
+    
+}
