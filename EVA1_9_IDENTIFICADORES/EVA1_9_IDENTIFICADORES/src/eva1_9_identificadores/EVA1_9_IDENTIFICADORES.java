37174213079edb41b0c473a8f06768206cdd7dc4
TheFictionRed <143562833+TheFictionRed@users.noreply.github.com>
2023-09-13 16:54:07 -0700

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,25 @@
+
+package eva1_9_identificadores;
+
+
+public class EVA1_9_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        //int 1nombre; mo es valido, empieza con un numero
+        int $nombre;
+        int _nombre;
+        int nombre1;
+        int nombre; 
+        int Nombre;
+        int nombre_dato;
+        //int nombre; la variable ya existe previamente 
+   
+    //int public; no podemos usar public, es palabra reservada (keyword)
+        int Public;
+        double lsdhajgfgfd;
+        
+    
+    }
+    
+    
+}
