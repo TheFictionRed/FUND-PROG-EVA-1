b54a0b1fccbcece9e2dce6ead98e9af0c15eaa2c
TheFictionRed <143562833+TheFictionRed@users.noreply.github.com>
2023-09-20 14:32:53 -0700

    Add files via upload

---
+++ EVA1_11_TIPO_DE_DATOS/src/eva1_11_tipo_de_datos/EVA1_11_TIPO_DE_DATOS.java
@@ -0,0 +1,42 @@
+
+package eva1_11_tipo_de_datos;
+
+import java.util.Scanner;
+
+public class EVA1_11_TIPO_DE_DATOS {
+
+    
+    public static void main(String[] args) {
+       byte cali;
+       Scanner sc = new Scanner(System.in);
+      byte num;
+       
+    cali = 127;
+       //cali = 200;
+    //cali = -128;
+       //cali = -200;   
+       //cali = 127 + 1;
+       
+    /*    System.out.println("Sumar valores");   
+       byte captu = sc.nextByte();
+       
+        num = (byte)(cali + captu);
+        System.out.println(num);
+       
+       int valor = Integer.MAX_VALUE;
+        System.out.println(valor + 1);
+        valor = Integer.MIN_VALUE;
+        System.out.println(valor);   */
+        
+        
+        //----------------------
+        char caracter = '@';
+        System.out.println(caracter);
+        System.out.println(caracter + 1);
+            
+        
+        
+    
+    }
+    
+}
